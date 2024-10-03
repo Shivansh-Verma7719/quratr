be0ce90dc66651061149752a952f8a94c7e8acb9
Shivansh Verma <shivanshverma082004@gmail.com>
2024-10-03 18:02:13 +0530

    fixed logout

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,26 +1,17 @@
-"use server";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from "next/server";
 
-export async function logout() {
-    const supabase = createClient();
-  
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.log(error)
-      redirect("/error");
-    }
-  
-    return 'Logged out';
-}
+export async function GET() {
+  const supabase = createClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        const result = await logout();
-        res.status(200).send(result);
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.log(error);
+    return NextResponse.redirect('/error');
+  }
+
+  const redirectTo = "/error";
+  return NextResponse.redirect(redirectTo);
 }
