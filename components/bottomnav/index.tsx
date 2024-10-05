d1bea297b81374bc24d206229b55c06812a9b4b3
Shivansh Verma <shivanshverma082004@gmail.com>
2024-10-05 22:32:55 +0530

    fixed discover page for phone

--- components/bottomnav/index.tsx
+++ components/bottomnav/index.tsx
@@ -2,19 +2,16 @@
 
 import React from "react";
 import Link from "next/link";
-import useScrollingEffect from "@/hook/use-scroll";
 import { useTheme } from "next-themes";
 import { BottomPages } from "../pages";
 
 const BottomNav = () => {
-  const scrollDirection = useScrollingEffect(); // Use the custom hook
-  const navClass = scrollDirection === "up" ? "" : "opacity-25 duration-500";
   const pages = BottomPages();
   const { theme } = useTheme();
 
   return (
     <div
-      className={`fixed bottom-0 w-full py-2 z-10 bg-background border-t border-zinc-200 shadow-lg sm:hidden ${navClass}`}
+      className={`fixed bottom-0 w-full py-2 z-10 bg-background border-t border-zinc-200 shadow-lg sm:hidden`}
     >
       <div className="flex flex-row justify-around items-center bg-transparent w-full">
         {pages.map((page, index) => (
