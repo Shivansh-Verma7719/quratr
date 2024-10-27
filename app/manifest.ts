9c61709e312be117fa17cd082b4f9e52efb67b5b
Shivansh Verma <shivanshverma082004@gmail.com>
2024-10-27 20:45:00 +0530

    manifest updated:shortcuts svg

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,6 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#000000",
     theme_color: "#000000",
+    lang: "en",
     icons: [
       {
         src: "/favicon.ico",
