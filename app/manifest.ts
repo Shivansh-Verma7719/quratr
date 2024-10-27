df0a4bf253a7c743bf9d977622036011385bc9b0
Shivansh Verma <shivanshverma082004@gmail.com>
2024-10-27 18:30:40 +0530

    manifest updated

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,28 +2,29 @@ import { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Quratr',
-    short_name: 'Quratr',
-    description: 'Quratr - Your ultimate platform for curated experiences.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#000000',
-    theme_color: '#000000',
+    id: "/",
+    name: "Quratr",
+    short_name: "Quratr",
+    description: "Quratr - Your ultimate platform for curated experiences.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: '16x16',
-        type: 'image/x-icon',
+        src: "/favicon.ico",
+        sizes: "16x16, 32x32, 48x48, 64x64",
+        type: "image/x-icon",
       },
       {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   }
