249b28f1e971a0bd68961c1e9de046fa2006f0ee
Shivansh Verma <shivanshverma082004@gmail.com>
2024-11-16 12:15:35 +0530

    max width added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
